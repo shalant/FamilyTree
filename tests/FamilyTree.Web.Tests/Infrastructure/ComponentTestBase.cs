f9d334e4a31b44c37cfcf283c68adc353592ea66
shalant <doug.rosenberg@gmail.com>
2026-08-16 22:19:45 -0500

    minor fix to test suite

--- tests/FamilyTree.Web.Tests/Infrastructure/ComponentTestBase.cs
+++ tests/FamilyTree.Web.Tests/Infrastructure/ComponentTestBase.cs
@@ -14,6 +14,7 @@ public abstract class ComponentTestBase : TestContext
         Services.AddSingleton<ToastService>();
         Services.AddSingleton<FamilyTreeLayoutEngine>();
         Services.AddSingleton<TreeContextService>();
+        Services.AddSingleton<ThemeService>();
 
         // MudBlazor services
         Services.AddMudServices();
